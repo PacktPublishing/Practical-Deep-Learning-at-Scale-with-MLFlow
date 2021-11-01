34e51ed89d85e79a3c67a39e8526a3801e29fd74
Yong Liu <yong.liu@outreach.io>
2021-10-31 21:06:23 -0700

    add fine_tuning function

---
+++ chapter04/README.md
@@ -0,0 +1,20 @@
+# Instructions for Chapter 4
+
+## Set up conda virtual environment dl_model by running:
+   1. conda create -n dl_model python==3.8.10
+   2. pip install -r requirements.txt
+   3. pip uninstall datasets
+   4. pip install datasets==1.2.1
+## Set up a local full-fledged MLflow tracking and registry server with MySQL as backend storage and MinIO as the artifact store
+#### The mlflow docker setup is based on `https://github.com/sachua/mlflow-docker-compose` with updates on latest versions of images of nginix and minio and python version to 3.8. Also fixed bugs for creating buckets using minIO.
+   1. Checkout the entire repo and switch to folder `chapter03/mlflow_docker_setup`
+   2. Run `bash start_mlflow.sh` to start the mlflow server
+      
+      a) You must have docker running in your local environment in order to successfully run the docker-compose command
+      
+      b) Please refer to https://docs.docker.com/engine/install/ to install either Docker Desktop or a linux distr of docker. 
+      
+      c) Once the docker is installed, start it so it is running in the background.
+   3. See the MLflow server UI at `http://localhost`
+   4. See the object store (minIO, a multi-cloud object store) UI at `http://localhost:9000`
+   5. When you are done, run `bash stop_mlflow.sh` to stop the local mlflow server
---
+++ chapter04/fine_tuning.py
@@ -0,0 +1,56 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import flash
+import mlflow
+import torch
+from flash.core.data.utils import download_data
+from flash.text import TextClassificationData, TextClassifier
+import torchmetrics
+
+# %%
+download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
+datamodule = TextClassificationData.from_csv(
+    input_fields="review",
+    target_fields="sentiment",
+    train_file="data/imdb/train.csv",
+    val_file="data/imdb/valid.csv",
+    test_file="data/imdb/test.csv"
+)
+
+
+# %%
+classifier_model = TextClassifier(backbone="prajjwal1/bert-tiny",
+                                  num_classes=datamodule.num_classes, metrics=torchmetrics.F1(datamodule.num_classes))
+trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
+
+
+# %%
+EXPERIMENT_NAME = "dl_model_chapter04"
+mlflow.set_tracking_uri('http://localhost')
+mlflow.set_experiment(EXPERIMENT_NAME)
+experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
+print("experiment_id:", experiment.experiment_id)
+REGISTERED_MODEL_NAME = 'dl_finetuned_model'
+
+
+# %%
+mlflow.pytorch.autolog()
+with mlflow.start_run(experiment_id=experiment.experiment_id, run_name="chapter04") as dl_model_tracking_run:
+    trainer.finetune(classifier_model, datamodule=datamodule, strategy="freeze")
+    trainer.test()
+
+    # mlflow log additional hyper-parameters used in this training
+    mlflow.log_params(classifier_model.hparams)
+
+
+# %%
+run_id = dl_model_tracking_run.info.run_id
+print("run_id: {}; lifecycle_stage: {}".format(run_id,
+                                               mlflow.get_run(run_id).info.lifecycle_stage))
+
+
+# %%
+# register the fine-tuned model
+logged_model = f'runs:/{run_id}/model'
+mlflow.register_model(logged_model, REGISTERED_MODEL_NAME)
