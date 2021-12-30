6bd0348143efedf5a911fe2332ab3528e12723f4
Yong Liu <yong.liu@outreach.io>
2021-12-29 16:11:33 -0800

    chapter05 start

---
+++ chapter05/main.py
@@ -0,0 +1,48 @@
+import logging
+import os
+
+import click
+import mlflow
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+_steps = [
+    "download_data",
+    "fine_tuning_model",
+    "register_model"
+]
+
+
+@click.command()
+@click.option("--steps", default="all", type=str)
+def run_pipeline(steps):
+
+    # Setup the mlflow experiment and AWS access
+    os.environ["MLFLOW_TRACKING_URI"] = "http://localhost"
+    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+    os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+
+    EXPERIMENT_NAME = "dl_model_chapter05"
+    mlflow.set_experiment(EXPERIMENT_NAME)
+    experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
+    logger.info("pipeline experiment_id: %s", experiment.experiment_id)
+
+    # Steps to execute
+    active_steps = steps.split(",") if steps != "all" else _steps
+    logger.info("pipeline active steps to execute in this run: %s", active_steps)
+
+    with mlflow.start_run(run_name='pipeline', experiment_id=experiment.experiment_id, nested=True) as active_run:
+        if "download_data" in active_steps:
+            download_run = mlflow.run(".", "download_data", experiment_id=experiment.experiment_id, parameters={})
+            download_run = mlflow.tracking.MlflowClient().get_run(download_run.run_id)
+            file_path_uri = download_run.data.params['local_folder']
+            logger.info('downloaded data is located locally in folder: %s', file_path_uri)
+            logger.info(download_run)
+
+    logger.info('finished mlflow pipeline run with a run_id = %s', active_run.info.run_id)
+
+
+if __name__ == "__main__":
+    run_pipeline()
