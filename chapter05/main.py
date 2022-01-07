6894a71aabb36e6fae6bbd5b2129e3aca639fd26
Yong Liu <yong.liu@outreach.io>
2022-01-06 18:00:00 -0800

    remove hardcoded environment variables for remote execution

--- chapter05/main.py
+++ chapter05/main.py
@@ -19,12 +19,12 @@ _steps = [
 def run_pipeline(pipeline_steps):
 
     # Setup the mlflow experiment and AWS access
-    os.environ["MLFLOW_TRACKING_URI"] = "http://localhost"
-    os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
-    os.environ["AWS_ACCESS_KEY_ID"] = "minio"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+    # os.environ["MLFLOW_TRACKING_URI"] = "http://localhost"
+    # os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+    # os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+    # os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
 
-    EXPERIMENT_NAME = "dl_model_chapter05"
+    EXPERIMENT_NAME = "/Shared/dl_model_chapter05"
     mlflow.set_experiment(EXPERIMENT_NAME)
     experiment = mlflow.get_experiment_by_name(EXPERIMENT_NAME)
     logger.info("pipeline experiment_id: %s", experiment.experiment_id)
