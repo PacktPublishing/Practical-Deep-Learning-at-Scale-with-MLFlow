b15f4694e94d7da199365f1af90837e3682f2da4
Yong Liu <yong.liu@outreach.io>
2022-03-06 21:13:29 -0800

    clean up code

--- chapter08/batch/batch_inference.py
+++ chapter08/batch/batch_inference.py
@@ -1,13 +1,12 @@
 import mlflow
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
-from pyspark.sql.functions import struct
 
 spark = SparkSession.builder.appName("Batch inference with MLflow DL inference pipeline").getOrCreate()
 
 # load a logged model with run id and model name
 # logged_model = 'runs:/37b5b4dd7bc04213a35db646520ec404/inference_pipeline_model'
-# load a registered model with a version number
+# or load a registered model with a version number
 #  model_uri=f"models:/{model_name}/{model_version}"
 
 logged_model = 'models:/inference_pipeline_model/6'
@@ -24,7 +23,3 @@ df = df.select('review').withColumnRenamed('review', 'text')
 df = df.withColumn('predictions', loaded_model())
 
 df.show(n = 10, truncate=80, vertical=True)
-
-# export SPARK_HOME=/Users/yongliu/spark_server/spark-3.0.1-bin-hadoop2.7
-# export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.9-src.zip:$PYTHONPATH
-# export PATH=$SPARK_HOME/bin:$SPARK_HOME/python:$PATH
