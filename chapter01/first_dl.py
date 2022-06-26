a5561c4d94b42b4f2910c091fe7ee6695108490f
Yong Liu <yong.liu@outreach.io>
2022-06-26 15:48:30 -0700

    added leading print statements

--- chapter01/first_dl.py
+++ chapter01/first_dl.py
@@ -3,6 +3,7 @@ import flash
 from flash.core.data.utils import download_data
 from flash.text import TextClassificationData, TextClassifier
 
+print('### download IMDb data to local folder')
 download_data("https://pl-flash-data.s3.amazonaws.com/imdb.zip", "./data/")
 datamodule = TextClassificationData.from_csv(
     input_fields="review",
@@ -12,17 +13,23 @@ datamodule = TextClassificationData.from_csv(
     test_file="data/imdb/test.csv"
 )
 
+print('### define a text classifier ')
 classifier_model = TextClassifier(backbone="prajjwal1/bert-tiny", num_classes=datamodule.num_classes)
+
+print('### define the trainer')
 trainer = flash.Trainer(max_epochs=3, gpus=torch.cuda.device_count())
+
+print('### fine tune the pretrained model to get a new model for sentiment classification')
 trainer.finetune(classifier_model, datamodule=datamodule, strategy="freeze")
 
+print('### get prediction outputs for two sample sentences')
 predictions = classifier_model.predict(
     [
         "Best movie I have seen.",
         "What a movie!",
     ]
 )
-
 print(predictions)
 
+print('### get classifier test results')
 trainer.test()
